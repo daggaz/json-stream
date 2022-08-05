f5e68345ac33b398d56f2e5f52174208ca55cc6e
smheidrich <smheidrich@weltenfunktion.de>
2022-08-05 12:54:11 +0200

    Allow passing custom tokenizer to load/visit

--- src/json_stream/loader.py
+++ src/json_stream/loader.py
@@ -2,7 +2,7 @@ from json_stream.base import StreamingJSONBase
 from json_stream.tokenizer import tokenize
 
 
-def load(fp, persistent=False):
-    token_stream = tokenize(fp)
+def load(fp, persistent=False, tokenizer=tokenize):
+    token_stream = tokenizer(fp)
     _, token = next(token_stream)
     return StreamingJSONBase.factory(token, token_stream, persistent)
