1b76609b1498c03170b86e5665981dda6fd17cb1
Jamie Cockburn <jamiec@extramsp.com>
2023-02-11 11:01:21 +0000

    use to_standard_types to allow json.dump of transient objects

--- src/json_stream/dump/tests/test_dump.py
+++ src/json_stream/dump/tests/test_dump.py
@@ -10,16 +10,32 @@ class TestDump(TestCase):
     JSON = '{"count": 3, "results": ["a", "b", "c"]}'
 
     def test_dump_default(self):
-        data = json_stream.load(StringIO(self.JSON), persistent=True)
+        data = json_stream.load(StringIO(self.JSON))
         output = json.dumps(data, default=default)
         self._assert_json_okay(output)
 
     def test_dump_cls(self):
-        data = json_stream.load(StringIO(self.JSON), persistent=True)
+        data = json_stream.load(StringIO(self.JSON))
         output = json.dumps(data, cls=JSONStreamEncoder)
         self._assert_json_okay(output)
 
     def test_dump_context(self):
+        data = json_stream.load(StringIO(self.JSON))
+        with JSONStreamEncoder():
+            output = json.dumps(data)
+        self._assert_json_okay(output)
+
+    def test_dump_default_persistent(self):
+        data = json_stream.load(StringIO(self.JSON), persistent=True)
+        output = json.dumps(data, default=default)
+        self._assert_json_okay(output)
+
+    def test_dump_cls_persistent(self):
+        data = json_stream.load(StringIO(self.JSON), persistent=True)
+        output = json.dumps(data, cls=JSONStreamEncoder)
+        self._assert_json_okay(output)
+
+    def test_dump_context_persistent(self):
         data = json_stream.load(StringIO(self.JSON), persistent=True)
         with JSONStreamEncoder():
             output = json.dumps(data)
