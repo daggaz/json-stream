bc4ee4ffc7b871fda7807287abbc51f4c31adccd
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2023-03-17 22:23:56 +0000

    added integration test for unterminated string in rust tokenizer

--- src/json_stream/tests/test_tokenizer_integration.py
+++ src/json_stream/tests/test_tokenizer_integration.py
@@ -1,6 +1,9 @@
+from io import StringIO
 from unittest import skipUnless
 
 import json_stream_rs_tokenizer
+
+from json_stream import load
 from json_stream.tokenizer import tokenize
 
 from json_stream.select_tokenizer import default_tokenizer
@@ -30,3 +33,7 @@ class TestRSTokenizer(JSONLoadTestCase):
         self.assertIs(default_tokenizer, json_stream_rs_tokenizer.RustTokenizer)
         obj = {"a": 1, "b": None, "c": True}
         self._test_object(obj, persistent=False, binary=True)
+
+    def test_unterminated_string(self):
+        with self.assertRaisesRegex(ValueError, "Unterminated string"):
+            load(StringIO('["unterminated')).read_all()
